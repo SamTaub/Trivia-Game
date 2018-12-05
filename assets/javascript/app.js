33e1e07e30cda9237c1b3abb9ac3f021f3b1426c
Sam Taub <staub91@gmail.com>
2018-12-05 14:20:54 -0500

    fixed variable definitions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,47 @@
+var triviaQuestions = [
+    {
+        question: "In 1970 the Pittsburgh Steelers won 4 Super Bowls with their help of their tremedous defensive line.  What was their nickname?",
+        questionOptions: ["Yellow Curtain", "Steel Hammers", "Steel Curtain", "Fearsome Four"],
+        questionAnswer: 2,
+    },
+    {
+        question: "In 2012, which two brothers coached against eachother in Super Bowl XLVII (47)?",
+        questionOptions: ["John & Jim Harbaugh", "John & Trey Gruden", "John & Trey Madden", "John & Chuck Pagano"],
+        questionAnswer: 0,
+    },
+    {
+        question: "Which current NFC West Team was part of the AFC until the league realigned in 2002?",
+        questionOptions: ["Los Angeles Rams", "Seattle Seahawks", "Arizona Cardinals", "San Francisco 49ers"],
+        questionAnswer: 1,
+    },
+    {
+        question: "Which team lost 4 consecutive Superbowls (XXV to XXVII) between 1991 and 1994?",
+        questionOptions: ["Minnesota Vikings", "Denver Broncos", "Cincinnati Bengals", "Buffalo Bills"],
+        questionAnswer: 3,
+    },
+    {
+        question: 'Which team was nicknamed "The Great Show on Turf" between 1999 and 2001?',
+        questionOptions: ["Atlanta Falcons", "New Orleans Saints", "Dallas Cowboys", "St. Louis Rams"],
+        questionAnswer: 3,
+    },
+    {
+        question: "After the 1983 Season, the Colts moved to Indianapolis from which city?",
+        questionOptions: ["Baltimore", "Phoenix", "Los Angeles", "Houston"],
+        questionAnswer: 0,
+    },
+    {
+        question: "In 2002, the NFL added what team as it's 32nd team?",
+        questionOptions: ["Baltimore Ravens", "Carolina Panthers", "Houston Texans", "Jacksonville Jaguars"],
+        questionAnswer: 2,   
+    },
+    {
+        question: "Every year since 1945, what team has hosted a home game on Thanksgiving Day?",
+        questionOptions: ["Green Bay Packers", "Detroit Lions", "New York Giants", "Chicago Bears"],
+        questionAnswer: 1,
+    }
+];
+
 //Counter Variables
 let correctCounter = 0;
 let incorrectCounter = 0;
 let unansweredCounter = 0;
-
-//Question Variables
-let questionOne = "In the 1970's, the Pittsburgh Steelers won 4 Super Bowls with the help of a tremendous defensive line with which nickname?"
-let questionTwo = "In 2012, which two brothers coached against eachother in Super Bowl XLVII (47)?"
-let questionThree = "Which current NFC West Team was part of the AFC up until the year 2002?"
-let questionFour = "Which team lost 4 consecutive Superbowls (XXV to XXVII) between 1991 and 1994?"
-let questionFive = 'Which team was nicknamed "The Great Show on Turf" between 1999 and 2001?'
-let questionSix =  "After the 1983 Season, the Colts moved to Indianapolis from which city?"
-let questionSeven = "in 2002 the NFL added it's 32nd team to the league.  This team is?"
-let questionEight = "Every year since 1945, what team has hosted a home game on Thanksgiving Day?"
-
-let questionArray = ["In the 1970's, the Pittsburgh Steelers won 4 Super Bowls with the help of a tremendous defensive line with which nickname?", "In 2012, which two brothers coached against eachother in Super Bowl XLVII (47)?", "Which current NFC West Team was part of the AFC up until the year 2002?", "Which team lost 4 consecutive Superbowls (XXV to XXVII) between 1991 and 1994?", 'Which team was nicknamed "The Great Show on Turf" between 1999 and 2001?', "After the 1983 Season, the Colts moved to Indianapolis from which city?", "in 2002 the NFL added it's 32nd team to the league.  This team is?", "Every year since 1945, what team has hosted a home game on Thanksgiving Day?"]
-
-
-
-//Timer Function
-
-
-
-$("#start-game-button").click(function (){
-    $("#start-game-button").hide();
-    $("#trivia-question").text(questionOne);
-});
